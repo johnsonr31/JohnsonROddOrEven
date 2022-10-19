d153bc54304cc8d656c27bb7ffb5d900604fae44
ricRobo4 <artistr3@proton.me>
2022-10-18 21:07:51 -0700

    created the while loop and added some text

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.Clear();
 
 bool playAgain = true;
+
+while(playAgain == true)
+{
+    Console.WriteLine("Would you like to know if a number is odd or even?");
+    Console.Write("YES or NO: ");
+}
